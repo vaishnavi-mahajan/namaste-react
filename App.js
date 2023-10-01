c372f7db0e177a43ae4de79f8b7de300c4cf6c37
vaishnavi-mahajan <vaishnavimahajan019@gmail.com>
2023-09-30 19:18:57 -0700

    Episode 2: Igniting our app

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-
+import React from "react"
+import ReactDOM from "react-dom";
 
 const parent=React.createElement("div",
 {id:"Parent"},
 React.createElement("div",{id:"child"},
-[ React.createElement("h1",{},"I am h1 tag"),
+[ React.createElement("h1",{},"This is namaste react 😅"),
 React.createElement("h2",{},"I am h2 tag")
 ]),
 React.createElement("div",{id:"child2"},
